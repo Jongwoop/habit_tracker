532dcbdcc79ae6f6a424e9b686cb8bc3d525676c
Jongwoop <58589069+Jongwoop@users.noreply.github.com>
2021-05-05 13:41:13 +0300

    component: Habits

---
+++ src/components/habit.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Habit extends Component {
+  handleIncrement = (event) => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecrement = (event) => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
+  handleDelete = (event) => {};
+
+  render() {
+    const { name, count } = this.props.habit;
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
